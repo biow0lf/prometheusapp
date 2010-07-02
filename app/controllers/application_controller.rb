7bd69818ecb99b2cbee018ba8d8706e5304c4258
Igor Zubkov <igor.zubkov@gmail.com>
2010-07-02 03:24:39 +0300

    set locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  before_filter :set_locale
+
+  def set_locale
+#    available = %w{en ru uk}
+#    params[:locale] ||= request.preferred_language_from(available)
+#    params[:locale] ||= 'en'
+    I18n.locale = params[:locale]
+#    set_locale params[:locale]
+  end
 end
+
