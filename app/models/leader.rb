b34d91e46d9981a29b1a346aea3935b86f281359
Igor Zubkov <igor.zubkov@gmail.com>
2010-07-07 02:48:32 +0300

    update model

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -10,4 +10,18 @@ class Leader < ActiveRecord::Base
                                                                 :vendor => vendor } ) == 1
   end
 
+  def self.update_from_gitalt(vendor, branch, url)
+    ActiveRecord::Base.transaction do
+      ActiveRecord::Base.connection.execute("DELETE FROM leaders WHERE branch = '" + branch + "' AND vendor = '" + vendor + "'")
+
+      file = open(URI.escape(url)).read
+
+      file.each_line do |line|
+        package = line.split[0]
+        login = line.split[1]
+        Leader.create :package => package, :login => login, :branch => branch, :vendor => vendor
+      end
+    end
+  end
+
 end
--- test/unit/leader_test.rb
+++ test/unit/leader_test.rb
@@ -9,9 +9,19 @@ class Test::Unit::TestCase
       assert_equal(Leader.count(:all), 1)
     end
   end
+
+  def self.should_have_9882_leaders
+    should "have 9882 leaders in database" do
+      Leader.delete_all
+      Leader.update_from_gitalt('ALT Linux', 'Sisyphus', 'test/data/list.packages.sisyphus')
+      assert_equal(Leader.count(:all), 15879)
+    end
+  end
+
 end
 
 class LeaderTest < ActiveSupport::TestCase
   should_validate_presence_of :package, :login, :branch, :vendor
   should_check_leader_for_exists_and_not_add_twice
+  should_have_9882_leaders
 end
