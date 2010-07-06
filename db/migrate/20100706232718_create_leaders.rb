283652e3c6bab618aa5f8e4a3b893aea2a9f6cf4
Igor Zubkov <igor.zubkov@gmail.com>
2010-07-07 02:30:01 +0300

    add leader migrate script

---
+++ db/migrate/20100706232718_create_leaders.rb
@@ -0,0 +1,16 @@
+class CreateLeaders < ActiveRecord::Migration
+  def self.up
+    create_table :leaders do |t|
+      t.string :package
+      t.string :login
+      t.string :branch
+      t.string :vendor
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :leaders
+  end
+end
