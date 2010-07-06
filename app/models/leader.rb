df7b5681e0e6c07579861058260576c8eb4a1e8c
Igor Zubkov <igor.zubkov@gmail.com>
2010-07-07 02:39:15 +0300

    update leader model

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -1,3 +1,13 @@
 class Leader < ActiveRecord::Base
   validates_presence_of :package, :login, :branch, :vendor
+  validate :uniqueness_of_package_login_branch_vendor
+
+  def uniqueness_of_package_login_branch_vendor
+    errors.add(:uniq, "should be uniq") if Leader.count(:all, :conditions => {
+                                                                :package => package,
+                                                                :login => login,
+                                                                :branch => branch,
+                                                                :vendor => vendor } ) == 1
+  end
+
 end
--- test/unit/leader_test.rb
+++ test/unit/leader_test.rb
@@ -1,5 +1,17 @@
 require 'test_helper'
 
+class Test::Unit::TestCase
+  def self.should_check_leader_for_exists_and_not_add_twice
+    should "check acl for exists and not add twice" do
+      Leader.delete_all
+      Leader.create(:package => 'glibc', :login => 'ldv', :vendor => 'ALT Linux', :branch => 'Sisyphus')
+      Leader.create(:package => 'glibc', :login => 'ldv', :vendor => 'ALT Linux', :branch => 'Sisyphus')
+      assert_equal(Leader.count(:all), 1)
+    end
+  end
+end
+
 class LeaderTest < ActiveSupport::TestCase
   should_validate_presence_of :package, :login, :branch, :vendor
+  should_check_leader_for_exists_and_not_add_twice
 end
