279742e946c156e00ab80742b672b537b632b79e
Igor Zubkov <igor.zubkov@gmail.com>
2010-07-07 07:11:03 +0400

    add bygroup action to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,15 @@ class HomeController < ApplicationController
     @groups = Group.find_groups_in_sisyphus
   end
 
+  def bygroup
+    @group = params[:group]
+    @group = @group + '/' + params[:group2] if !params[:group2].nil?
+    @group = @group + '/' + params[:group3] if !params[:group3].nil?
+
+    @srpms = Srpm.all :conditions => {
+                        :group => @group,
+                        :branch => 'Sisyphus',
+                        :vendor => 'ALT Linux' }
+  end
+
 end
