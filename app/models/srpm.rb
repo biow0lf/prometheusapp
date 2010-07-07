1a6b6c198d9246c8768e9351ba96ec567fcc877a
Igor Zubkov <igor.zubkov@gmail.com>
2010-07-07 03:27:00 +0300

    update srpm model

--- app/models/srpm.rb
+++ app/models/srpm.rb
@@ -1,3 +1,15 @@
 class Srpm < ActiveRecord::Base
   validates_presence_of :branch, :vendor, :filename, :name, :version, :release
+  validate :uniqueness_of_srpm
+
+  def uniqueness_of_srpm
+    errors.add(:uniq, "should be uniq") if Srpm.count(:all, :conditions => {
+                                                              :branch => branch,
+                                                              :vendor => vendor,
+                                                              :filename => filename,
+                                                              :name => name,
+                                                              :version => version,
+                                                              :release => release } ) == 1
+  end
 end
+
--- test/unit/srpm_test.rb
+++ test/unit/srpm_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
+class Test::Unit::TestCase
+  def self.should_check_srpm_for_exists_and_not_add_twice
+    should "check srpm for exists and not add twice" do
+      Srpm.delete_all
+      Srpm.create(:branch => 'Sisyphus', :vendor => 'ALT Linux', :filename => 'glibc-2.11.2-alt2.src.rpm', :name => 'glibc', :version => '2.11.2', :release => 'alt2')
+      Srpm.create(:branch => 'Sisyphus', :vendor => 'ALT Linux', :filename => 'glibc-2.11.2-alt2.src.rpm', :name => 'glibc', :version => '2.11.2', :release => 'alt2')
+      assert_equal(Srpm.count(:all), 1)
+    end
+  end
+
+end
+
 class SrpmTest < ActiveSupport::TestCase
   should_validate_presence_of :branch, :vendor, :filename, :name, :version, :release
   should_have_db_index :branch, :vendor, :name
+  should_check_srpm_for_exists_and_not_add_twice
 end
