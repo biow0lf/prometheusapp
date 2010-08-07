433675e4003b4152f618ec45196bb283dd616b97
Igor Zubkov <igor.zubkov@gmail.com>
2010-08-07 17:16:37 +0300

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  layout 'application'
-  before_filter :set_locale
-
-  def set_locale
-    params[:locale] ||= 'en'
-    I18n.locale = params[:locale]
-    I18n.locale = 'pt-br' if params[:locale] == 'br'
-  end
+  before_filter :set_gettext_locale
+#  layout 'application'
+#  before_filter :set_locale
+#
+#  def set_locale
+#    params[:locale] ||= 'en'
+#    I18n.locale = params[:locale]
+#    I18n.locale = 'pt-br' if params[:locale] == 'br'
+#  end
 end
 
